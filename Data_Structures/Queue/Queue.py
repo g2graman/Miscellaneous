3426f490d9dc94bcaacf532436023ab59e6621c6
g2graman <fran.gramano@mail.utoronto.ca>
2014-02-19 22:08:20 -0500

    Added Stack ADT and Queue ADT structures.

---
+++ Data_Structures/Queue/Queue.py
@@ -0,0 +1,31 @@
+class Queue(object): 
+  def __init__(self): 
+    self.length = 0 
+    self.items = []
+    self.head = 0
+
+  def is_empty(self): 
+    return (self.length - self.head <= 0) 
+
+
+  def enqueue(self, element):
+    self.items.append(element)
+    self.length += 1 
+
+
+  def dequeue(self):
+    old_head = self.head
+    if (self.length > 0):
+      self.head += 1
+    else:
+      if (self.head <= self.length):
+        if (self.length == 0):
+          self.items = []
+          self.head = 0
+    return self.items[old_head]
+
+
+  def __repr__(self):
+    if(0 <= self.head < self.length):
+      return str(self.items[self.head:])
+    return str([])
---
+++ Data_Structures/Stack/Stack.py
@@ -0,0 +1,24 @@
+class Stack(object): 
+  def __init__(self) : 
+    self.elements = []
+    self.length = 0
+
+
+  def push(self, element): 
+    self.elements.append(element)
+    self.length += 1 
+
+
+  def pop(self):
+  	if (not(self.is_empty())):
+  		self.length -= 1
+    	return self.elements.pop()
+
+
+  def peek(self):
+  	if (not(self.is_empty())):
+  		return self.elements[0]
+
+
+  def is_empty(self): 
+    return self.length == 0
