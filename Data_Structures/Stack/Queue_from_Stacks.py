b3daa8a70cf991511062f37e25b308fe4f9e010e
g2graman <fran.gramano@mail.utoronto.ca>
2014-02-19 22:24:35 -0500

    Implemented Queue from two stacks.

---
+++ Data_Structures/Stack/Queue_from_Stacks.py
@@ -0,0 +1,27 @@
+from Stack import *
+
+
+class Queue_from_Stacks(object): 
+  def __init__(self): 
+    self.in_stack = Stack()
+    self.out_stack = Stack()
+    self.length = 0
+
+
+  def is_empty(self): 
+    return self.length <= 0 
+
+
+  def enqueue(self, element):
+    self.in_stack.push(element)
+    self.length += 1 
+
+
+  def dequeue(self):
+    if(self.out_stack.is_empty()):
+      while (not(self.in_stack.is_empty())):
+        self.out_stack.push(self.in_stack.pop())
+    if (not(self.out_stack.is_empty())):
+      self.length -= 1
+      return self.out_stack.pop()
+
